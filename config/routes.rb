cf0093b35d51cec793fbac9c5b268406ab16a711
nergdvnlt <tylerlundgren@yahoo.com>
2018-05-23 18:13:34 -0600

    Building out sessions, adding log out user test.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[new create]
 
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
   get '/:slug', to: 'users#show', as: 'user'
 end
