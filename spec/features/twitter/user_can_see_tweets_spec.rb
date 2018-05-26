a08ea2e2a6cddb93c97eabeefc76c0eae4bb4f74
nergdvnlt <tylerlundgren@yahoo.com>
2018-05-26 15:44:13 -0600

    First user story built around ideas about what I want on the page.

---
+++ spec/features/twitter/user_can_see_tweets_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'User selects currency' do
+  scenario 'and sees tweets associated with that currency' do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    # When I visit a userpage
+    visit "/#{user.slug}"
+
+    select 'Bitcoin', from: 'currency[id]'
+    click_on 'Get Tweets!'
+
+    expect(current_path).to eq(tweets_path)
+    expect(page).to have_css('.tweet', count: 10)
+  end
+end
