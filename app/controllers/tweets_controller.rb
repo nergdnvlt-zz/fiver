9d2e052a9737ce5db2205698d803074b68eff326
nergdvnlt <tylerlundgren@yahoo.com>
2018-05-26 17:20:51 -0600

    Got auth implemented, getting requests from the Twitter API.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,12 @@
 class TweetsController < ApplicationController
   def index
-    crypto = Crypto.find(params[:crypto_id])
+    crypto_name = Crypto.find(params[:crypto_id]).name
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV['twitter_consumer_key']
+      config.consumer_secret = ENV['twitter_consumer_secret']
+      config.access_token = ENV['twitter_access_token']
+      config.access_token_secret = ENV['twitter_access_token_secret']
+    end
+    @tweets = client.search("#{crypto_name} -rt", result_type: "recent", lang: "en").take(10)
   end
 end
