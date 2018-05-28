a867839b39db47a8b6fa9bdb20eed437af9071be
nergdvnlt <tylerlundgren@yahoo.com>
2018-05-28 16:40:15 -0600

    Adding single page.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,4 +3,8 @@ class TweetsController < ApplicationController
     crypto_name = Crypto.find(params[:crypto_id]).name
     @analyzer = AnalyzerPresenter.new(crypto_name)
   end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
 end
