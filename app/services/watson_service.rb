8ebd6a43442a3f772fbb1d852bdef26d8b9c31ab
nergdvnlt <tylerlundgren@yahoo.com>
2018-05-28 14:18:57 -0600

    Testing watson service.

--- app/services/watson_service.rb
+++ app/services/watson_service.rb
@@ -3,6 +3,24 @@ class WatsonService
     @raw_tweets = raw_tweets
   end
 
+  def analyzed_tweets
+    analysis_service[:sentences_tone].map do |sentence|
+      next if sentence[:tones].empty?
+      Tweet.create({ text: sentence[:text],
+                     score: sentence[:tones][0][:score],
+                     tone_id: sentence[:tones][0][:tone_id],
+                     tone_name: sentence[:tones][0][:tone_name] })
+    end.compact
+  end
+
+  def document_tones
+    @document_tones = new_document_analysis[:document_tone][:tones].map do |tone|
+      Tone.new(tone)
+    end
+  end
+
+  private
+
   def conn
     Faraday.new(url: ENV['watson_base_url']) do |req|
       req.basic_auth(ENV['watson_username'], ENV['watson_password'])
@@ -24,24 +42,7 @@ class WatsonService
     @analysis ||= JSON.parse(post_to_watson.body, symbolize_names: true)
   end
 
-  def analyzed_tweets
-    analysis_service[:sentences_tone].map do |sentence|
-      next if sentence[:tones].empty?
-      Tweet.create({ text: sentence[:text],
-                     score: sentence[:tones][0][:score],
-                     tone_id: sentence[:tones][0][:tone_id],
-                     tone_name: sentence[:tones][0][:tone_name] })
-    end.compact
-  end
-
   def new_document_analysis
     @new_document_analysis ||= JSON.parse(post_to_watson(analyzed_tweets).body, symbolize_names: true)
-  end
-
-
-  def document_tones
-    @document_tones = new_document_analysis[:document_tone][:tones].map do |tone|
-      Tone.new(tone)
-    end
   end
 end
