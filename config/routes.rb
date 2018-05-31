4b181036dbfee5ec90837530e3ca0fd1b907a444
nergdvnlt <tylerlundgren@yahoo.com>
2018-05-31 12:27:20 -0600

    Adding some styling before seperating some things.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
 
   get '/tweets', to: 'tweets#index'
 
-
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy', as: 'logout'
