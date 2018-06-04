82189bb4b7e41b2878c2806133973542c3d2e2a0
nergdvnlt <tylerlundgren@yahoo.com>
2018-06-04 09:26:43 -0600

    Trying to fix travis.

--- spec/features/api_calls/user_can_visit_single_tweet_page_spec.rb
+++ spec/features/api_calls/user_can_visit_single_tweet_page_spec.rb
@@ -2,20 +2,18 @@ require 'rails_helper'
 
 feature 'User selects currency' do
   scenario 'and sees tweets associated with that currency' do
-    VCR.use_cassette('/features/user_sees_single_feed_back') do
-      Crypto.create!(name: 'Bitcoin', symbol: 'BTC')
-      tone = Tone.create!(tone_name: 'Joy', url: 'https://i.imgur.com/s9HLKk5.png')
-      tweet = Tweet.create!(text: 'bitcoin ftw')
-      TweetTone.create!(tweet: tweet, tone: tone)
+    Crypto.create!(name: 'Bitcoin', symbol: 'BTC')
+    tone = Tone.create!(tone_name: 'Joy', url: 'https://i.imgur.com/s9HLKk5.png')
+    tweet = Tweet.create!(text: 'bitcoin ftw')
+    TweetTone.create!(tweet: tweet, tone: tone)
 
-      user = create(:user)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-      visit "/tweets/#{tweet.id}"
+    visit "/tweets/#{tweet.id}"
 
-      expect(page).to have_css('.tone-name')
-      expect(page).to have_content('Tweet')
-      expect(page).to have_content('Tones')
-    end
+    expect(page).to have_css('.tone-name')
+    expect(page).to have_content('Tweet')
+    expect(page).to have_content('Tones')
   end
 end
