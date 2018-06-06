def71391bb2eaa06040d7a5b05ea9b07117bc98b
nergdvnlt <tylerlundgren@yahoo.com>
2018-06-06 10:41:13 -0600

    Fixing last tests.

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -1,33 +1,42 @@
-# require 'rails_helper'
-#
-# describe Document do
-#   # let(:input_string){ "Are there any functional BTC plugins for WooCommerce that don't use 3rd party vendors?Bill Regulating Blockchains Dies In Committee@PaulManafort They weren't after him: there were concerns about some of his investment activities/fun… https://t.co/JlxNdOYLQz$BTC#bitcoin 1hr looking bearish all the way down to soft support at 7200.am happy to follow this project because my project is in line with the thinking and what I want #Crypto… https://t.co/k8rEcMFki1If#Bitcoin is a #Bubble, Final Stage of ‘#Panic’ on the #Horizon https://t.co/Ba7fnXOiPxto open bitcoin exchange in Japan https://t.co/SJ86VkRAYYhttps://t.co/u0n93m0xDL / Bitcoin Token  #Airdrop  🌕🌕🌕👏    Join in this Airdrop of 2000 BTK  Europe: Apple Co-Founder Hedges His Bets On Bitcoin https://t.co/X0LtoElKhcTelegram" }
-#
-#   describe 'class methods' do
-#     it '#as_json' do
-#       VCR.use_cassette('/models/document_class-BTC') do
-#         tones = Document.as_json(1)
-#
-#         expect(tones).to be_a Hash
-#         expect(tones.keys).to eq([:tones])
-#         expect(tones[:tones]).to be_a Array
-#         expect(tones[:tones][0].keys).to eq([:tone_name])
-#         expect(tones[:tones][0].values).to eq(['Joy'])
-#       end
-#     end
-#   end
-#
-#   describe 'instance methods' do
-#     it 'constructs json' do
-#       VCR.use_cassette('/models/document_instance-BTC') do
-#         tones = Document.new(input_string).as_json
-#
-#         expect(tones).to be_a Hash
-#         expect(tones.keys).to eq([:tones])
-#         expect(tones[:tones]).to be_a Array
-#         expect(tones[:tones][0].keys).to eq([:tone_name])
-#         expect(tones[:tones][0].values).to eq(['Joy'])
-#       end
-#     end
-#   end
-# end
+require 'rails_helper'
+
+describe Document do
+  before(:each) do
+    tone = Tone.create!(tone_name: 'Anger')
+    @crypto = Crypto.create!(name: 'Bitcoin', symbol: 'BTC')
+    @crypto.tweets.create!(text: 'bitcoin ftw', tone: tone)
+    @crypto.tweets.create!(text: 'yay', tone: tone)
+    @crypto.tweets.create!(text: 'wonderbread', tone: tone)
+    @crypto.tweets.create!(text: 'winning', tone: tone)
+    @crypto.tweets.create!(text: 'losing', tone: tone)
+    @crypto.tweets.create!(text: 'tired', tone: tone)
+    @crypto.tweets.create!(text: 'appalachian', tone: tone)
+    @crypto.tweets.create!(text: 'thrasher', tone: tone)
+    @crypto.tweets.create!(text: 'fluffy', tone: tone)
+    @crypto.tweets.create!(text: 'dynamic', tone: tone)
+  end
+  
+  describe 'class methods' do
+    it '#as_json' do
+      tones = Document.as_json(@crypto.id)
+
+      expect(tones).to be_a Hash
+      expect(tones.keys).to eq([:tones])
+      expect(tones[:tones]).to be_a Array
+      expect(tones[:tones][0].keys).to eq([:tone_name])
+      expect(tones[:tones][0].values).to eq(['Anger'])
+    end
+  end
+
+  describe 'instance methods' do
+    it 'constructs json' do
+      tones = Document.new(@crypto.id).as_json
+
+      expect(tones).to be_a Hash
+      expect(tones.keys).to eq([:tones])
+      expect(tones[:tones]).to be_a Array
+      expect(tones[:tones][0].keys).to eq([:tone_name])
+      expect(tones[:tones][0].values).to eq(['Anger'])
+    end
+  end
+end
