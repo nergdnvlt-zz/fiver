a4dde63121e37458f000f490e753faa14ee44582
nergdvnlt <tylerlundgren@yahoo.com>
2018-06-05 21:40:17 -0600

    Working on sessions acting strange.

--- spec/features/user/user_can_login_to_sessions_spec.rb
+++ spec/features/user/user_can_login_to_sessions_spec.rb
@@ -20,8 +20,17 @@ feature 'An existing user' do
 
   scenario 'can log back out' do
     user1 = create(:native_user)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
-    visit "/#{user1.slug}"
+    visit '/'
+
+    within('.welcome-login') do
+      click_on 'Login'
+    end
+
+    fill_in 'Username', with: user1.username
+    fill_in 'Password', with: user1.password
+
+    click_on 'Login'
+    expect(current_path).to eq(user_path(user1.slug))
 
     click_on 'Log Out'
 
