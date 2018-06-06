c8e4ed8956b236bc9b60dc9479b95481a665ca3d
nergdvnlt <tylerlundgren@yahoo.com>
2018-06-05 20:09:20 -0600

    REfactoring document.

--- app/controllers/api/v1/watson_controller.rb
+++ app/controllers/api/v1/watson_controller.rb
@@ -2,16 +2,6 @@ class Api::V1::WatsonController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    render json: make_doc(request.body.string).as_json
-  end
-
-  private
-
-  def service_call(input_string)
-    WatsonService.new(input_string).analysis_service
-  end
-
-  def make_doc(input_string)
-    Document.new(service_call(input_string))
+    render json: Document.as_json(request.body.string)
   end
 end
